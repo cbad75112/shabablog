fe2afc1e629c807a6e2a4ff01897426cc5df2598
F853 <f853@ktgh.com.tw>
2025-10-27 16:42:11 +0800

    deploy new

---
+++ docs/assets/use-quasar-CSzWwFYe.js
@@ -0,0 +1,1 @@
+import{a,bt as r}from"./index-Diujp8v7.js";function t(){return a(r)}export{t as u};
--- docs/assets/vite-register-B9SWT0_T.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./index-ua6OBQ-0.js";import{P as t}from"./index.esm-Bx2iyrBb.js";const s=o(({app:e})=>{e.use(t)});export{s as default};
